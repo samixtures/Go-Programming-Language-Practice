7e610ede7ab1252ea3d72416248a942037b41ded
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-03-01 15:26:23 -0800

    Initial commit

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+import "rsc.io/quote"
+
+func main() {
+    fmt.Println(quote.Go())
+}
+
+// Printed a nice quote: Don't communicate by sharing memory, share memory by communicating.
